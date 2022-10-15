fb5d7180d43e4a9541e4c53108b15a4506d138d8
Russell Matney <russell.matney@gmail.com>
2022-10-15 10:44:10 -0400

    wip: adds an in-memory cache to curl requests
    Caches repeated requests via curl with an in-memory cache.
    This allows the tests to make each request only once, and more tests can
    be written without encroaching further on third-party service
    rate-limits.
    
    Note that this in-memory cache lives for the life of the program, so
    long-running services that use neil as a library would need to
    periodically clear the cache, unless some other clearing mechanism is
    implemented.
    
    In this model, typical bb one-off usages should always start with an
    empty cache, so there should be little change for end-users.

--- src/babashka/neil/curl.clj
+++ src/babashka/neil/curl.clj
@@ -13,11 +13,31 @@
 (def dev-github-token (System/getenv "BABASHKA_NEIL_DEV_GITHUB_TOKEN"))
 
 (def curl-opts
-  (merge {:throw false
+  (merge {:throw      false
           :compressed (not (fs/windows?))}
          (when (and dev-github-user dev-github-token)
            {:basic-auth [dev-github-user dev-github-token]})))
 
-(defn curl-get-json [url]
+(defn do-curl-get-json [url]
   (-> (curl/get url curl-opts)
       :body (cheshire/parse-string true)))
+
+(def curl-cache
+  "An in-memory cache reducing repeated api calls.
+  Intended to support test code."
+  (atom {}))
+
+(defn clear-cache []
+  (reset! curl-cache {}))
+
+;; TODO flag via env var
+(def dev-cache-enabled true)
+
+(defn curl-get-json [url]
+  (if dev-cache-enabled
+    (if-let [cached (@curl-cache url)]
+      cached
+      (let [result (do-curl-get-json url)]
+        (swap! curl-cache assoc url result)
+        result))
+    (do-curl-get-json url)))
